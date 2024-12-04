848d6e4070d199c8fd087e57950ef8b7b28759d7
user <Benot Baguelin>
2024-12-04 16:28:19 +0000

    subprocess with &

--- src/repl.c
+++ src/repl.c
@@ -3,6 +3,14 @@
 #include "../include/get_exit_status.h"
 #include "../include/calculate_duration.h"
 
+#define MAX_BG_PROCESSES 128
+
+typedef struct {
+    pid_t pid;
+    char command[CMD_BUFFER_SIZE];
+    struct timespec start_time;
+    int active;
+} BackgroundProcess;
 
 void repl() {
     struct timespec timer_start, timer_finish;  
@@ -11,7 +19,10 @@ void repl() {
     ssize_t bytes_read;
     int status;
     size_t command_length = 0; // To track the length of the command read
-    char prompt[64] = "enseash % ";
+    char prompt[512] = "enseash % ";
+
+    int bg_count = 0;
+    BackgroundProcess bg_processes[MAX_BG_PROCESSES];
 
     while (1) {
         print(prompt);
@@ -38,10 +49,6 @@ void repl() {
 
         clock_gettime(CLOCK_REALTIME, &timer_start);
         pid_t pid = fork();
-        
-        char a[30];
-        sprintf(a, "pid : %d\n", pid);
-        print(a);
 
         if (pid < 0) {
             print("Fork failed\n");
@@ -51,19 +58,54 @@ void repl() {
         if (pid == 0) {
             // Child process: execute command
             if (strcmp(command, "") == 0) {
-                execl("/bin/sh", "sh", "-c", "date", (char *)NULL);
+                // execl("/bin/sh", "sh", "-c", "date", (char *)NULL);
             } else {
                 execl("/bin/sh", "sh", "-c", command, (char *)NULL);
             }
             exit(0);
-        } else {
-            // Parent process: wait for command to complete
-            wait(&status);
+        } else if (command_length > 0 && command[command_length - 1] == '&') {
+            // Parent process starting new process
+            bg_processes[bg_count].pid = pid;
+            strcpy(bg_processes[bg_count].command, command);
+            bg_processes[bg_count].start_time = timer_start;
+            bg_processes[bg_count].active = 1;
+            bg_count++;
+            sprintf(prompt, "enseash [%d&] %% ", pid);
+        } else { 
+            // Parent process
+            waitpid(pid, &status, 0);
             clock_gettime(CLOCK_REALTIME, &timer_finish);
             get_exit_status(status, exit_status);
+            int elapsed_ms = calculate_duration(timer_finish, timer_start);
+            sprintf(prompt, "enseash [%s|%dms] %% ", exit_status, elapsed_ms);
         }
 
-        int elapsed_ms = calculate_duration(timer_finish, timer_start); 
-        sprintf(prompt, "enseash [%s|%dms] %% ", exit_status, elapsed_ms);
+        for (int i = 0; i < bg_count; i++) { 
+            if (bg_processes[i].active) {
+                int bg_status; 
+                char bg_exit_status[30];
+                struct timespec bg_finish;
+                
+    
+                pid_t result = wait4(bg_processes[i].pid, &bg_status, WNOHANG, NULL); // non blocking
+
+                if (result > 0) { 
+                    bg_processes[i].active = 0; 
+                    clock_gettime(CLOCK_REALTIME, &bg_finish);
+                    
+                    int elapsed_ms = calculate_duration(bg_finish, bg_processes[i].start_time);
+                    get_exit_status(bg_status, bg_exit_status);
+                    char process_priority = i == bg_count - 1 ? '+' : (i == bg_count - 2 ? '-' : ' ');
+                    
+                    sprintf(prompt, "[%d]%c Ended: %s\nenseash [%s|%dms] %% ", 
+                        i + 1, // process number 
+                        process_priority, // priority indicator + or - or nothing
+                        bg_processes[i].command, // the command that was executed
+                        bg_exit_status, // the process exit status
+                        elapsed_ms // execution time
+                    );
+                }
+            }
+        }
     }
 }
